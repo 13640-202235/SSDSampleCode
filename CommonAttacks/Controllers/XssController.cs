e74b0854bfbae178582ba97ea0b947a241d3b1a9
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-10-06 00:34:16 -0400

    Common Attacks - Part 1

---
+++ CommonAttacks/Controllers/XssController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommonAttacks.Controllers
+{
+    public class XssController : Controller
+    {
+        public IActionResult DangerousGreeting(string name)
+        {
+            string rawGreeting = $"Welcome {name} to our application!";
+            return Content(rawGreeting, "text/html");
+        }
+
+        public IActionResult SafeGreeting(string name)
+        {
+            string rawGreeting = $"Welcome {name} to our application!";
+            string safeGreeting = System.Net.WebUtility.HtmlEncode(rawGreeting);
+            return Content(safeGreeting, "text/html");
+        }
+
+        public IActionResult SafeView(string name)
+        {
+            ViewBag.Message = $"Welcome {name} to our application!";
+            return View();
+        }
+
+        public IActionResult DangerousView(string name)
+        {
+            ViewBag.Message = $"Welcome {name} to our application!";
+            return View();
+        }
+    }
+}
