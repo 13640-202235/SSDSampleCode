0242a0b8daf28cd1554ed46ad1c8bacce67c2e61
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-10-20 14:56:46 -0400

    Common Attacks - Part 3

--- CommonAttacks/Controllers/HomeController.cs
+++ CommonAttacks/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CommonAttacks.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace CommonAttacks.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
