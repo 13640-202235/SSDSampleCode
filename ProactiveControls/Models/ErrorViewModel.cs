7a9d2f4fe3ef21e63785e04f1b65c689dfc6180d
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-10-20 22:29:23 -0400

    Proactive Controls - Part 1

---
+++ ProactiveControls/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProactiveControls.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
